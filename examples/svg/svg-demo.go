57849b6b18fba78c7f165160082ca9e43a1020ef
Ebtisam Jubair <abirmridha2005@gmail.com>
2025-12-31 18:08:56 +0600

    chore: commit all generated example images and fix pathing

--- examples/svg/svg-demo.go
+++ examples/svg/svg-demo.go
@@ -10,13 +10,13 @@ import (
 
 func main() {
 	fmt.Println("Creating SVG demo...")
-	
+
 	// Create a simple SVG document programmatically
 	createSVGDocument()
-	
+
 	// Create a drawing and export to SVG
 	createDrawingAndExportSVG()
-	
+
 	fmt.Println("SVG demo completed!")
 }
 
@@ -44,60 +44,60 @@ func createSVGDocument() {
 </svg>`
 
 	// Save SVG file
-	file, err := os.Create("demo.svg")
+	file, err := os.Create("images/svg/demo.svg")
 	if err != nil {
 		fmt.Printf("Error creating SVG file: %v\n", err)
 		return
 	}
 	defer file.Close()
-	
+
 	_, err = file.WriteString(svgContent)
 	if err != nil {
 		fmt.Printf("Error writing SVG file: %v\n", err)
 		return
 	}
-	
-	fmt.Println("Created demo.svg")
+
+	fmt.Println("Created images/svg/demo.svg")
 }
 
 func createDrawingAndExportSVG() {
 	// Create a drawing using the graphics library
 	dc := advancegg.NewContext(500, 400)
-	
+
 	// Background
 	dc.SetRGB(0.95, 0.95, 1.0)
 	dc.Clear()
-	
+
 	// Title
 	dc.SetRGB(0, 0, 0)
 	dc.DrawString("Graphics to SVG Export", 150, 30)
-	
+
 	// Draw some shapes
 	dc.SetRGB(1, 0.2, 0.2)
 	dc.DrawCircle(100, 100, 40)
 	dc.Fill()
-	
+
 	dc.SetRGB(0.2, 0.8, 0.2)
 	dc.DrawRectangle(200, 60, 80, 80)
 	dc.Fill()
-	
+
 	dc.SetRGB(0.2, 0.2, 1)
 	dc.DrawEllipse(400, 100, 50, 30)
 	dc.Fill()
-	
+
 	// Draw some lines
 	dc.SetRGB(0.5, 0, 0.8)
 	dc.SetLineWidth(3)
 	dc.MoveTo(50, 200)
 	dc.LineTo(450, 200)
 	dc.Stroke()
-	
+
 	// Draw a curve
 	dc.SetRGB(0.8, 0.4, 0)
 	dc.MoveTo(50, 250)
 	dc.QuadraticTo(250, 220, 450, 250)
 	dc.Stroke()
-	
+
 	// Draw a complex path
 	dc.SetRGB(0.6, 0.2, 0.8)
 	dc.SetLineWidth(2)
@@ -109,35 +109,35 @@ func createDrawingAndExportSVG() {
 	dc.LineTo(350, 280)
 	dc.LineTo(400, 300)
 	dc.Stroke()
-	
+
 	// Save as PNG first
-	advancegg.SavePNG("graphics-demo.png", dc.Image())
-	fmt.Println("Created graphics-demo.png")
-	
+	advancegg.SavePNG("images/svg/graphics-demo.png", dc.Image())
+	fmt.Println("Created images/svg/graphics-demo.png")
+
 	// Create equivalent SVG manually (since full SVG export is complex)
 	svgFromGraphics := createSVGFromDrawing()
-	
-	file, err := os.Create("graphics-export.svg")
+
+	file, err := os.Create("images/svg/graphics-export.svg")
 	if err != nil {
 		fmt.Printf("Error creating SVG export file: %v\n", err)
 		return
 	}
 	defer file.Close()
-	
+
 	_, err = file.WriteString(svgFromGraphics)
 	if err != nil {
 		fmt.Printf("Error writing SVG export file: %v\n", err)
 		return
 	}
-	
-	fmt.Println("Created graphics-export.svg")
+
+	fmt.Println("Created images/svg/graphics-export.svg")
 }
 
 func createSVGFromDrawing() string {
 	// Manually create SVG equivalent of the drawing
 	// In a full implementation, this would be automated
 	var svg strings.Builder
-	
+
 	svg.WriteString(`<?xml version="1.0" encoding="UTF-8"?>
 <svg width="500" height="400" viewBox="0 0 500 400" xmlns="http://www.w3.org/2000/svg">
   <!-- Background -->
@@ -165,7 +165,7 @@ func createSVGFromDrawing() string {
   <path d="M 100 300 L 150 280 L 200 300 L 250 280 L 300 300 L 350 280 L 400 300" 
         fill="none" stroke="#9933cc" stroke-width="2"/>
 </svg>`)
-	
+
 	return svg.String()
 }
 
